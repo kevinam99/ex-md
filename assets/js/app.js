a4be36dc78c3109729e1a9d7530ad8e9035980b7
Kevin <kevinam99@gmail.com>
2025-05-09 12:00:11 +0530

    Trim whitespaces

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,7 @@ Hooks.CopyToClipboard = {
       const markdownOutput = document.getElementById("markdown-output")
       
       // Copy the HTML content to clipboard
-      navigator.clipboard.writeText(markdownOutput.textContent)
+      navigator.clipboard.writeText(markdownOutput.textContent.trim())
         .then(() => {
           // Trigger a flash message on success
           this.pushEvent("copied_to_clipboard")
